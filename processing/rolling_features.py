8c2630fd63e2d8fb567df304291dcf9b6c0d31a3
Paulo-Pereira346 <paulpereira220@gmail.com>
2026-03-24 23:24:38 +0530

    Added cumulative features

--- processing/rolling_features.py
+++ processing/rolling_features.py
@@ -7,14 +7,22 @@ df = pd.read_csv("../data/processed/epl_pre_rolling.csv")
 # df_temp = df[df["team"] == "Arsenal"]
 # print(df_temp["date"].is_monotonic_increasing)
 
+
+#Rolling Features over 5,10 windows
 windows = [5,10]
-attributes = ["gf","ga","gd","points","win","sot","cs"]
-for att in attributes:
+rolling_attributes = ["gf","ga","gd","points","win","sot","cs"]
+for att in rolling_attributes:
     for i in windows:
         col = f"{att}_last_{i}"
-        df[col] = df.groupby("team")[att].transform(lambda x: x.shift(1).rolling(window=i,min_periods=1).mean())
+        df[col] = df.groupby("team")[att].transform(lambda x: x.shift(1).rolling(window=i,min_periods=1).mean()) 
+        #Shift is required to prevent data leakage
+        
+cumulative_attributes = ["gf","ga","points"]
+for att in cumulative_attributes:
+    col = f"cum_{att}"
+    df[col] = df.groupby("team")[att].transform(lambda x: x.shift(1).expanding().mean())
 
-print(df[df["team"]=="Man United"][["date","gf","gf_last_5","gf_last_10"]].head(11))
+print(df[df["team"]=="Arsenal"][["date","gf","cum_gf"]])
 # print(df.head())
 
 
